63d0b9b6018f6b5745541362340970f6c61ed8c5
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-06-30 20:40:29 +0400

    add action

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,8 @@ const spriteSVG = require('./gulp/tasks/spriteSVG');
 const serve = require('./gulp/tasks/serve');
 const spritePNG = require('./gulp/tasks/spritePNG');
 const webP = require('./gulp/tasks/webP');
+const { argv } = require('yargs');
 
 const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, webP, spriteSVG, spritePNG, fonts);
 
-exports.default = gulp.series(
-  clean,
-  dev,
-  serve
-);
+exports.default = argv.prod ? gulp.series(clean, dev) : gulp.series(clean, dev, serve);
